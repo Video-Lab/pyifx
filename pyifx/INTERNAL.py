c7591de7a1b960e94852892bbb6891a6e7571abd
video-lab <32363370+Video-Lab@users.noreply.github.com>
2019-07-06 09:40:59 -0500

    v0.0.1 - Main internal functions

--- pyifx/INTERNAL.py
+++ pyifx/INTERNAL.py
@@ -1,13 +1,12 @@
 import os
 import sys
-import cv2
 import numpy as np
 import matplotlib as mpl
 
 class _PyfixImage():
 	def __init__(self, path,image=None):
 		self.path = path
-		self.image = cv2.imread(path)
+		self.image = mpl.pyplot.imread(path, os.path.splitext(path)[1])
 
 	@classmethod
 	def from_image(cls, image):
@@ -17,7 +16,7 @@ class _PyfixImage():
 
 
 def _check_path_type(path):
-	is os.path.isdir(path):
+	if os.path.isdir(path):
 		return 'dir'
 	elif os.path.isfile(path):
 		return 'file'
@@ -25,15 +24,16 @@ def _check_path_type(path):
 		return None
 
 def _convert_dir_to_images(dir):
-		images = []
-	 	possible_extensions = ['.tif', '.tiff', '.jpg', '.jpeg', '.png', '.raw']
+	images = []
+	possible_extensions = ['.tif', '.tiff', '.jpg', '.jpeg', '.png', '.raw']
 
-	 	def add_to_images(dir):
-			for f in os.listdir(dir):
-		 		if os.path.splitext(f)[1] in possible_extensions:
-		 			images.append(cls(os.path.join(dir,f)))
-		 		elif os.path.isdir(f):
-		 			add_to_images(f)
+	def add_to_images(idir):
+		for f in os.listdir(idir):
+		 	if os.path.splitext(f)[1] in possible_extensions:
+		 		images.append(cls(os.path.join(idir,f)))
+		 	elif os.path.isdir(f):
+		 		add_to_images(f)
 
-	 	return images
+	add_to_images(dir)
+	return images
 
