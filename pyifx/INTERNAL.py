d8ddc279f47661b6849e65d146de844311d2841a
video-lab <32363370+Video-Lab@users.noreply.github.com>
2019-07-06 09:16:09 -0500

    Add constructors for pyfix image

--- pyifx/INTERNAL.py
+++ pyifx/INTERNAL.py
@@ -4,5 +4,13 @@ import cv2
 import numpy as np
 import matplotlib as mpl
 
+class _PyfixImage():
+	def __init__(self, path):
+		self.path = path
+		self.image = cv2.imread(path)
 
+	@classmethod
+	def from_image(cls, image):
+		return cls(None,image)
 
+	
