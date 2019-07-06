ebec32228dc9979a1357a0722ba170eaa0667032
video-lab <32363370+Video-Lab@users.noreply.github.com>
2019-07-06 09:19:06 -0500

    Add check path type function

--- pyifx/INTERNAL.py
+++ pyifx/INTERNAL.py
@@ -13,4 +13,11 @@ class _PyfixImage():
 	def from_image(cls, image):
 		return cls(None,image)
 
-	
+
+def checkPathType(path):
+	is os.path.isdir(path):
+		return 'dir'
+	elif os.path.isfile(path):
+		return 'file'
+	else:
+		return None
