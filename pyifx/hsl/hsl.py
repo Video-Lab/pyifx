4dff17e0500e631db7945cbf7b24db0c75690de4
video-lab <32363370+Video-Lab@users.noreply.github.com>
2019-07-09 17:19:31 -0500

    Revert back to old algorithm + changes

--- pyifx/hsl/hsl.py
+++ pyifx/hsl/hsl.py
@@ -1,7 +1,7 @@
 import INTERNAL
 
-def brighten(img_paths,factor=0.35):
+def brighten(img_paths,factor=0.45):
 	INTERNAL._brightness(img_paths, factor, "b")
 
-def darken(img_paths,factor=0.35):
+def darken(img_paths,factor=0.45):
 	INTERNAL._brightness(img_paths, factor, "d")
