635fa64621414b85285c55e27fcd95f88d8d8d63
video-lab <32363370+Video-Lab@users.noreply.github.com>
2019-07-10 12:04:52 -0500

    Clamp values for all external functions to range

--- pyifx/hsl/hsl.py
+++ pyifx/hsl/hsl.py
@@ -1,10 +1,10 @@
 import INTERNAL
 
 def brighten(img_paths,factor=0.45):
-	INTERNAL._brightness(img_paths, factor, "b")
+	INTERNAL._brightness(img_paths, max(0, min(factor, 1)), "b")
 
 def darken(img_paths,factor=0.45):
-	INTERNAL._brightness(img_paths, factor, "d")
+	INTERNAL._brightness(img_paths, max(0, min(factor, 1)), "d")
 
 def color_overlay(img_paths, color, opacity=30):
-	INTERNAL._color_overlay(img_paths, color, opacity/100)
+	INTERNAL._color_overlay(img_paths, color, max(0, min(opacity, 100))/100)
