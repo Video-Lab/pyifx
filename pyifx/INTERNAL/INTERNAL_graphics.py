4d0c5051bba7b32212c4c121c4ac7fd5e898c131
video-lab <32363370+Video-Lab@users.noreply.github.com>
2019-07-12 18:11:31 -0500

    Declare functions for blur & convolution framework

---
+++ pyifx/INTERNAL/INTERNAL_graphics.py
@@ -0,0 +1,11 @@
+def _create_kernel(size, type=None):
+	pass
+
+def _is_matrix_symmetrical(kernel):
+	pass
+
+def _convolute(img, filter):
+	pass
+
+def _blur(img_paths, kernel):
+	pass
---
+++ pyifx/graphics/graphics.py
@@ -0,0 +1,2 @@
+def blur_gaussian(img_paths, radius=1.5):
+	pass
