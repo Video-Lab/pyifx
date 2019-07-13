6b1b0edbcccfd49c70380c30765940978c0c1ecf
video-lab <32363370+Video-Lab@users.noreply.github.com>
2019-07-13 09:49:49 -0500

    Gaussian kernel function

--- pyifx/INTERNAL/INTERNAL_graphics.py
+++ pyifx/INTERNAL/INTERNAL_graphics.py
@@ -1,5 +1,19 @@
-def _create_kernel(size, type=None):
-	pass
+def _create_kernel(size=(3,3), radius=1.5, type_kernel=None):
+	matrix = None
+
+	if type_kernel == "gaussian":
+	    m,n = [(ss-1.)/2. for ss in size]
+	    y,x = np.ogrid[-m:m+1,-n:n+1]
+	    matrix = np.exp( -(x*x + y*y) / (2.*radius*radius) )
+	    matrix[ matrix < np.finfo(matrix.dtype).eps*matrix.max() ] = 0
+	    sumh = matrix.sum()
+	    if sumh != 0:
+	        matrix /= sumh
+	    matrix = np.asmatrix(matrix)
+
+	else:
+		raise Exception("Something went wrong. Please try again.")
+
 
 def _is_matrix_symmetrical(kernel):
 	pass
