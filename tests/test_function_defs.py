2648cefcce8ec3739250e27e59f8f8f4fb35da67
video-lab <32363370+Video-Lab@users.noreply.github.com>
2019-07-14 16:03:18 -0500

    Add tests

--- tests/test_function_defs.py
+++ tests/test_function_defs.py
@@ -30,156 +30,6 @@ def _convert_dir_to_images(dirc):
 	add_to_images(dirc)
 	return images
 
-def _change_light(img, factor, method):
-	
-	for row in range(len(img.image)):
-		for p in range(len(img.image[row])):
-			for v in range(len(img.image[row][p])):
-				value = img.image[row][p][v]
-				if method == "b":
-					img.image[row][p][v] = min(255, value*(1+factor)-(value/6))
-				elif method == "d":
-					img.image[row][p][v] = max(0, value*(1-factor)+(value/6))
-				else:
-					raise Exception("Something went wrong. Please try again.")
-
-	out_path, extension = os.path.splitext(img.output_path)
-	file_count = 1
-	temp_path = out_path
-
-	while os.path.isfile(out_path + extension):
-		out_path = temp_path
-		out_path += f" ({file_count})"
-		file_count += 1
-
-	imageio.imwrite(out_path + extension, img.image)
-	return img
-
-def _brightness(img_paths, factor, method):
-		if type(img_paths) == ImageVolume:
-
-			if not os.path.exists(img_paths.odir):
-				os.makedirs(img_paths.odir)
-
-			new_imgs = img_paths.volume
-
-			for img in new_imgs:
-				if method == "b" or method == "d":
-					_change_light(img, factor, method)
-				else:
-					raise Exception("Something went wrong. Please try again.")
-
-		elif type(img_paths) == PyifxImage:
-				if method == "b" or method == "d":
-					_change_light(img_paths, factor, method)
-				else:
-					raise Exception("Something went wrong. Please try again.")
-
-		elif type(img_paths) == list:
-
-			for img in img_paths:
-				if type(img) != PyifxImage:
-					raise TypeError("Input contains non-Pyifx images and/or classes. Please try again.")
-				if method == "b" or method == "d":
-					_change_light(img, factor, method)
-				else:
-					raise Exception("Something went wrong. Please try again.")
-
-		else:
-			raise TypeError("Input contains non-Pyifx images and/or classes. Please try again.")
-
-
-def _color_overlay(img_paths, color, opacity):
-			if type(img_paths) == ImageVolume:
-
-				if not os.path.exists(img_paths.odir):
-					os.makedirs(img_paths.odir)
-
-				new_imgs = img_paths.volume
-
-				for img in new_imgs:
-					_add_color_overlay(img, color, opacity)
-
-			elif type(img_paths) == PyifxImage:
-				_add_color_overlay(img_paths, color, opacity )
-
-			elif type(img_paths) == list:
-
-				for img in img_paths:
-					if type(img) != PyifxImage:
-						raise TypeError("Input contains non-Pyifx images and/or classes. Please try again.")
-
-					_add_color_overlay(img, color, opacity)
-
-			else:
-				raise TypeError("Input contains non-Pyifx images and/or classes. Please try again.")
-
-def _add_color_overlay(img, color, opacity):
-	for row in range(len(img.image)):
-		for p in range(len(img.image[row])):
-			for v in range(len(img.image[row][p])):
-
-				val = img.image[row][p][v]
-				diff = color[v] - val
-				diff *= opacity
-				img.image[row][p][v] += diff
-
-
-	_write_file(img)
-	return img
-
-def _saturation(img_paths, percent, method):
-	if type(img_paths) == ImageVolume:
-
-		if not os.path.exists(img_paths.odir):
-			os.makedirs(img_paths.odir)
-
-		new_imgs = img_paths.volume
-
-		for img in new_imgs:
-			if method == "s" or method == "ds":
-				_saturate(img, percent, method)
-			else:
-				raise Exception("Something went wrong. Please try again.")
-
-	elif type(img_paths) == PyifxImage:
-
-		if method == "s" or method == "ds":
-			_saturate(img_paths, percent, method)
-
-		else:
-			raise Exception("Something went wrong. Please try again.")
-
-	elif type(img_paths) == list:
-
-		for img in img_paths:
-			if type(img) != PyifxImage:
-				raise TypeError("Input contains non-Pyifx images and/or classes. Please try again.")
-			if method == "s" or method == "ds":
-				_saturate(img, percent, method)
-			else:
-				raise Exception("Something went wrong. Please try again.")
-
-		else:
-			raise TypeError("Input contains non-Pyifx images and/or classes. Please try again.")
-
-def _saturate(img, percent, method):
-	type_map = {"s": 1, "ds": -1}
-
-	for row in range(len(img.image)):
-		for p in range(len(img.image[row])):
-
-			gray_val = sum(img.image[row][p])/3
-			for v in range(len(img.image[row][p])):
-
-				value = img.image[row][p][v]
-				diff = gray_val - value
-				pixel_change = diff * (type_map[method]*percent)
-				img.image[row][p][v] = max(0, min((img.image[row][p][v]-pixel_change), 255))
-	
-	_write_file(img)
-	return img
-
 def _write_file(img):
 	out_path, extension = os.path.splitext(img.output_path)
 
@@ -234,28 +84,25 @@ class ImageVolume():
 
 		return new_imgs
 
+def _combine(img1, img2, out_path):
+	if img1.shape[0]*img1.shape[0] <= img2.shape[0]*img2.shape[1]:
+		shape = img1.shape
+	else:
+		shape = img2.shape
 
+	new_img = np.empty(shape)
 
-# hsl.py
+	for r in range(len(img1.image)):
+		for p in range(len(img1.image[r])):
+			for c in range(len(img1.image[r][p])):
+				try:
+					new_img[r][p][c] = (img1.image[r][p][c]+img2.image[r][p][c])/2
+				except IndexError:
+					pass
 
-def brighten(img_paths,factor=0.45):
-	_brightness(img_paths, factor, "b")
-
-def darken(img_paths,factor=0.45):
-	_brightness(img_paths, factor, "d")
-
-def color_overlay(img_paths, color, opacity=0.3):
-	_color_overlay(img_paths, color, opacity)
-
-def saturate(img_paths, percent=30):
-	_saturation(img_paths, max(0, min(percent, 100))/100, "s")
-
-def desaturate(img_paths, percent=30):
-	_saturation(img_paths, max(0, min(percent, 100))/100, "ds")
-
-def to_grayscale(img_paths):
-	_saturation(img_paths, 1, "ds")
-
+	img = PyifxImage(None, out_path, False)
+	img.image = new_img.astype(np.uint8)
+	return img
 
 # graphics.py
 
@@ -276,6 +123,8 @@ def blur_mean(img_paths, radius=3):
 
 def _create_kernel(radius, type_kernel, size=(3,3)):
 
+def _create_kernel(radius, type_kernel, size=(3,3)):
+
 	if len(size) != 2:
 		raise ValueError("Incorrect tuple dimensions used.")
 
@@ -286,7 +135,9 @@ def _create_kernel(radius, type_kernel, size=(3,3)):
 		size = int(2*radius)
 		if size % 2 == 0:
 			size += 1
+
 		size = (size, size)
+
 		m,n = [(ss-1.)/2. for ss in size]
 		y,x = np.ogrid[-m:m+1,-n:n+1]
 		kernel = np.exp( -(x*x + y*y) / (2.*radius*radius) )
@@ -305,7 +156,6 @@ def _create_kernel(radius, type_kernel, size=(3,3)):
 	kernel = np.flip(kernel, axis=1)
 
 	kernel = _Kernel(kernel, _is_kernel_seperable(kernel))
-
 	return kernel
 
 
@@ -315,32 +165,61 @@ def _is_kernel_seperable(kernel):
 	else:
 		return False
 
-def _convolute(img, kernel):
-	if kernel.seperable == False or kernel.seperable == True:
 
-		k_height = math.floor(kernel.kernel.shape[0]/2)
-		k_width = math.floor(kernel.kernel.shape[1]/2)		
-		new_img = np.zeros(shape=img.image.shape)
+def _convolute_over_image(img, kernel):
+	new_img = np.empty(shape=img.image.shape)
+	k_height = math.floor(kernel.shape[0]/2)
+	k_width = math.floor(kernel.shape[1]/2)
 
-		for r in range(len(img.image)):
-			for p in range(len(img.image[r])):
-				for c in range(len(img.image[r][p])):
-					new_pixel_value = 0
+	if k_height == 0:
+		k_height = 1
+
+	if k_width == 0:
+		k_width = 1
+
+	for r in range(len(img.image)):
+		for p in range(len(img.image[r])):
+			for c in range(len(img.image[r][p])):
+				new_pixel_value = 0
+
+				if k_height == 1:
+					for row in range(-k_width, k_width+1):
+						try:
+							new_pixel_value += img.image[r+row][p+column][c]*kernel[0][column+k_height]
+						except IndexError:
+							pass
+
+				elif k_width == 1:
+					for row in range(-k_width, k_width+1):
+						try:
+							new_pixel_value += img.image[r+row][p+column][c]*kernel[row+k_width][0]
+						except IndexError:
+							pass
+							
+				else:
 					for column in range(-k_height, k_height+1):
 						for row in range(-k_width, k_width+1):
 							try:
-								new_pixel_value += img.image[r+row][p+column][c]*kernel.kernel[row+k_width][column+k_height]
+								new_pixel_value += img.image[r+row][p+column][c]*kernel[row+k_width][column+k_height]
 							except IndexError:
 								pass
 
+				new_img[r][p][c] = new_pixel_value	
 
-					new_img[r][p][c] = new_pixel_value
+	new_img = new_img.astype(np.uint8)
+	img.image = new_img
+	return img
 
-		new_img = new_img.astype(np.uint8)
-		img.image = new_img
-		return img
+def _convolute(img, kernel):
+	if kernel.seperable == False
+		return _convolute_over_image(img, kernel.kernel)
+	else:
+		imgs = []
+		for matrix in kernel.seperated_kernel:
+			imgs.append(_convolute_over_image(img, matrix))
+		return combine(imgs[0], imgs[1], img.output_path)
 
-def _blur(img_paths, radius, type_kernel, size=(3,3)):
+def _blur(img_paths, radius, type_kernel, size):
 
 	kernel = _create_kernel(radius, type_kernel, size)	
 
@@ -352,10 +231,10 @@ def _blur(img_paths, radius, type_kernel, size=(3,3)):
 		new_imgs = img_paths.volume
 
 		for img in new_imgs:
-			_blur_operation(img, kernel)
+			_blur_operation(img_paths, kernel)
 
 	elif type(img_paths) == PyifxImage:
-		_blur_operation(img_paths, kernel)
+		_blur_operation(img, kernel)
 
 	elif type(img_paths) == list:
 
@@ -382,8 +261,8 @@ class _Kernel:
 		self.seperable = seperable
 		if seperable == True:
 			self.seperated_kernel = np.array([kernel[0], [c[0] for c in kernel]])
-			self.kernel = kernel
+			self.kernel = None
 
 		else:
-			self.seperated_kernel = np.array([kernel[0], [c[0] for c in kernel]])
+			self.seperated_kernel = None
 			self.kernel = kernel
