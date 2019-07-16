c9c9e5b50cc1f178d297a6b5310a0ce44c00b7bf
video-lab <32363370+Video-Lab@users.noreply.github.com>
2019-07-16 12:53:27 -0500

    Undo seperability changes

--- pyifx/INTERNAL/INTERNAL_graphics.py
+++ pyifx/INTERNAL/INTERNAL_graphics.py
@@ -40,73 +40,31 @@ def _create_kernel(radius, type_kernel, size):
 
 	kernel = np.flip(kernel, axis=1)
 
-	kernel = _Kernel(kernel, _is_kernel_seperable(kernel))
 	return kernel
-
-
-def _is_kernel_seperable(kernel):
-	if int(_rank(kernel)) == 1:
-		return True
-	else:
-		return False
-
 
 def _convolute_over_image(img, kernel):
 	new_img = np.empty(shape=img.image.shape)
-
-	if len(kernel.shape) == 1:
-		k_width = math.floor(kernel.shape[0]/2)
-		k_height = 1
-
-	elif kernel.shape[1] == 1:
-		k_height = math.floor(kernel.shape[0]/2)
-		k_width = 1
-
-	else:
-		k_height = math.floor(kernel.shape[0]/2)
-		k_width = math.floor(kernel.shape[1]/2)
+	k_height = math.floor(kernel.shape[0]/2)
+	k_width = math.floor(kernel.shape[1]/2)
 
 	for r in range(len(img.image)):
 		for p in range(len(img.image[r])):
 			for c in range(len(img.image[r][p])):
+
 				new_pixel_value = 0
-
-				if k_height == 1:
+				for row in range(-k_height, k_height+1):
 					for column in range(-k_width, k_width+1):
-						try:
-							new_pixel_value += img.image[r][p+column][c]*kernel[0][column+k_width]
-						except IndexError:
-							pass
 
-				elif k_width == 1:
-					for row in range(-k_height, k_height+1):
 						try:
-							new_pixel_value += img.image[r+row][p][c]*kernel[row+k_height][0]
+							new_pixel_value += img.image[r+column][p+row][c]*kernel[row+k_height][column+k_width]
 						except IndexError:
 							pass
-							
-				else:
-					for row in range(-k_height, k_height+1):
-						for column in range(-k_width, k_width+1):
-							try:
-								new_pixel_value += img.image[r+column][p+row][c]*kernel[row+k_height][column+k_width]
-							except IndexError:
-								pass
 
 				new_img[r][p][c] = new_pixel_value	
 
 	new_img = new_img.astype(np.uint8)
 	img.image = new_img
 	return img
-
-def _convolute(img, kernel):
-	if kernel.seperable == False:	
-		return _convolute_over_image(img, kernel.kernel)
-	else:
-		imgs = []
-		for matrix in kernel.seperated_kernel:
-			imgs.append(_convolute_over_image(img, matrix))
-		return INTERNAL.misc._combine(imgs[0], imgs[1], img.output_path)
 
 def _blur(img_paths, radius, type_kernel, size):
 
@@ -134,24 +92,6 @@ def _blur(img_paths, radius, type_kernel, size):
 			_blur_operation(img, kernel)
 
 def _blur_operation(img, kernel):
-	new_img = _convolute(img, kernel)
+	new_img = _convolute_over_image(img, kernel)
 	INTERNAL_misc._write_file(new_img)
 	return new_img
-
-def _rank(A, atol=1e-13, rtol=0):
-    A = np.atleast_2d(A)
-    s = np.linalg.svd(A, compute_uv=False)
-    tol = max(atol, rtol * s[0])
-    rank = int((s >= tol).sum())
-    return rank
-
-class _Kernel:
-	def __init__(self, kernel, seperable):
-		self.seperable = seperable
-		if seperable == True:
-			self.seperated_kernel = [np.array(kernel[0]), np.array([[c[0]] for c in kernel])]
-			self.kernel = None
-
-		else:
-			self.seperated_kernel = None
-			self.kernel = kernel
