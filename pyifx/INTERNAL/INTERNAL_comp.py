d9084db2beaddcb91a40e7629c966ce657d452c8
video-lab <32363370+Video-Lab@users.noreply.github.com>
2019-07-18 16:19:11 -0500

    Define resize handler function

--- pyifx/INTERNAL/INTERNAL_comp.py
+++ pyifx/INTERNAL/INTERNAL_comp.py
@@ -1,7 +1,36 @@
 from INTERNAL import *
 
-def _resize_handler(img_paths, new_size):
-	pass
+def _resize_handler(img_paths, new_size, write=True):
+
+	if type(img_paths) == misc.ImageVolume:
+
+				if not os.path.exists(img_paths.odir):
+					os.makedirs(img_paths.odir)
+
+				new_vol = img_paths
+				new_vol.volume = [] 
+
+				for img in img_paths.volume:
+					new_vol.volume.append(_resize_operation(img, new_size, write=write))
+
+				return new_vol
+
+			elif type(img_paths) == misc.PyifxImage:
+				return _resize_operation(img_paths, new_size, write=write)
+
+			elif type(img_paths) == list:
+				new_imgs = []
+
+				for img in img_paths:
+					
+					if type(img) != misc.PyifxImage:
+						raise TypeError("Input contains non-Pyifx images and/or classes. Please try again.")
+
+					return new_imgs.append(_resize_operation(img, new_size, write=write))
+				return new_imgs
+
+			else:
+				raise TypeError("Input contains non-Pyifx images and/or classes. Please try again.")
 
 def _resize_operation(img, new_size):
 	pass
--- pyifx/comp/comp.py
+++ pyifx/comp/comp.py
@@ -1,7 +1,7 @@
 import INTERNAL
 
-def resize(img_paths, new_size):
+def resize(img_paths, new_size, write=True):
 	INTERNAL._type_checker(new_size, [str])
 	INTERNAL._type_checker(img_paths, [INTERNAL.misc.PyifxImage, INTERNAL.misc.ImageVolume, list])
 
-	INTERNAL._resize_handler(img_paths, new_size)	
+	return INTERNAL._resize_handler(img_paths, new_size, write)	
