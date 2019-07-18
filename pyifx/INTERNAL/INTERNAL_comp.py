bf0d3d94a26f34edb2e931cae65b8c5ee979a3d9
video-lab <32363370+Video-Lab@users.noreply.github.com>
2019-07-18 16:15:38 -0500

    Define outer resize function & declare inner functions

--- pyifx/INTERNAL/INTERNAL_comp.py
+++ pyifx/INTERNAL/INTERNAL_comp.py
@@ -1,1 +1,13 @@
 from INTERNAL import *
+
+def _resize_handler(img_paths, new_size):
+	pass
+
+def _resize_operation(img, new_size):
+	pass
+
+def _expand_operation(img, new_size):
+	pass
+
+def _compress_operation(img, new_size):
+	pass
