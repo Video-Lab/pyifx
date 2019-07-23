dafc61474bfbdd1efeeb53c28aed5911b222dcd3
video-lab <32363370+Video-Lab@users.noreply.github.com>
2019-07-23 18:54:34 -0500

    Error tests (type error checks)

--- tests/tests.py
+++ tests/tests.py
@@ -11,8 +11,18 @@ def set_paths(new_path):
 
 	img_vol.set_output_path(os.path.join(new_path, os.path.split(vol.get_output_path())[1]))
 
-	img1.set_output_path(os.path.join(new_path, os.path.split(img1.get_output_path())[1])
-	img2.set_output_path(os.path.join(new_path, os.path.split(img2.get_output_path())[1])
+	img1.set_output_path(os.path.join(new_path, os.path.split(img1.get_output_path())[1]))
+	img2.set_output_path(os.path.join(new_path, os.path.split(img2.get_output_path())[1]))
+
+def call_error_test(function, args):
+	try:
+		globals()[function](*args)
+	except Exception:
+		print("Error caught successfully.")
+		return True
+
+	print(f"ERROR NOT CAUGHT: {function}({args})")
+	return False
 
 img1 = pyifx.misc.PyifxImage("imgs/512x512-jpeg-1.jpg", "imgs/hsl/brightness/512x512-jpeg-1.jpg")
 img2 = pyifx.misc.PyifxImage("imgs/512x512-png-1.png", "imgs/hsl/brightness/512x512-png-1.png")
@@ -32,11 +42,23 @@ pyifx.hsl.darken(img2, percent=50)
 pyifx.hsl.brighten(img_vol, percent=70)
 pyifx.hsl.darken(img_list, percent=70)
 
+call_error_test("pyifx.hsl.brighten", ["s", 50])
+call_error_test("pyifx.hsl.brighten", [img1, 's'])
+call_error_test("pyifx.hsl.brighten", [img1, 50, 's'])
+call_error_test("pyifx.hsl.darken", [img1, 50, 's'])
+call_error_test("pyifx.hsl.darken", ['s', 50])
+call_error_test("pyifx.hsl.darken", [img1, 50, 's'])
+
 set_paths("imgs/hsl/color_overlay/")
 
 pyifx.hsl.color_overlay(img1, [255,0,0], 60)
 pyifx.hsl.color_overlay(img_vol, [0,255,0], 60)
 pyifx.hsl.color_overlay(img_list, [0,0,255], 60)
+
+call_error_test("pyifx.hsl.color_overlay", ["asdf", [255,0,0], 60])
+call_error_test("pyifx.hsl.color_overlay", [img1, [255,0], 60])
+call_error_test("pyifx.hsl.color_overlay", [img1, [255,0,0], "s"])
+call_error_test("pyifx.hsl.color_overlay", [img1, [255,0,0], 60, "s"])
 
 set_paths("imgs/hsl/saturation/")
 
@@ -44,6 +66,13 @@ pyifx.hsl.saturate(img1, 70)
 pyifx.hsl.desaturate(img2, 60)
 pyifx.hsl.desaturate(img_vol, 30)
 pyifx.hsl.to_grayscale(img_list)
+
+call_error_test("pyifx.hsl.saturate", ["asdf", 70])
+call_error_test("pyifx.hsl.desaturate", ["asdf", 70])
+call_error_test("pyifx.hsl.to_grayscale", [10000])
+call_error_test("pyifx.hsl.saturate", [img1, "s"])
+call_error_test("pyifx.hsl.desaturate", [img1, "s"])
+
 
 # Composition
 
@@ -54,12 +83,23 @@ pyifx.comp.resize(img2, "256x256")
 pyifx.comp.resize(img_list, "694x1440")
 pyifx.comp.resize(img_vol, "1532x393")
 
+call_error_test("pyifx.comp.resize", [img1, "10241024"])
+call_error_test("pyifx.comp.resize", [img1, "1024xs"])
+call_error_test("pyifx.comp.resize", ["s", "1024x1024"])
+call_error_test("pyifx.comp.resize", [img1, 2])
+call_error_test("pyifx.comp.resize", [img1, "1024x1024", "s"])
+
 set_paths("imgs/comp/file_type/")
 
 pyifx.comp.change_file_type(img1, '.png')
 pyifx.comp.change_file_type(img2, '.jpg')
 pyifx.comp.change_file_type(img_list, 'png')
 pyifx.comp.change_file_type(img_vol, 'jpeg')
+
+call_error_test("pyifx.comp.change_file_type", [img1, "ekr"])
+call_error_test("pyifx.comp.change_file_type", [2, "png"])
+call_error_test("pyifx.comp.change_file_type", [2, "ekr"])
+call_error_test("pyifx.comp.change_file_type", [img1, "png", "s"]
 
 # Graphics
 
@@ -70,17 +110,32 @@ pyifx.graphics.blur_mean(img2, 3)
 pyifx.graphics.blur_gaussian(img_list, 1)
 pyifx.graphics.blur_mean(img_vol, 1)
 
+call_error_test("pyifx.graphics.blur_gaussian", ["s", 3])
+call_error_test("pyifx.graphics.blur_gaussian", [img1, "s"])
+call_error_test("pyifx.graphics.blur_gaussian", [img1, 2, (3,3), "s"])
+call_error_test("pyifx.graphics.blur_mean", ["s", 3])
+call_error_test("pyifx.graphics.blur_mean", [img1, "s"])
+call_error_test("pyifx.graphics.blur_mean", [img1, 2, 's'])
+
+
 set_paths("imgs/graphics/pixelate")
 
 pyifx.graphics.pixelate(img1, 4)
 pyifx.graphics.pixelate(img_list, 2)
 pyifx.graphics.pixelate(img_vol, 3)
 
+call_error_test("pyifx.graphics.pixelate", [img1, 's'])
+call_error_test("pyifx.graphics.pixelate", ['s', 4])
+call_error_test("pyifx.graphics.pixelate", [img1, 4, 's'])
+
 set_paths("imgs/graphics/edge")
 
 pyifx.graphics.detect_edges(img1)
 pyifx.graphics.detect_edges(img_list)
 pyifx.graphics.detect_edges(img_vol)
+
+call_error_test("pyifx.graphics.detect_edges", ['s'])
+call_error_test("pyifx.graphics.pixelate", [img1, 's'])
 
 set_paths("imgs/graphics/custom_convolution")
 
@@ -92,11 +147,17 @@ pyifx.graphics.convolute_custom(img1, sobel_horizontal_np)
 pyifx.graphics.convolute_custom(img_list, sobel_vertical)
 pyifx.graphics.convolute_custom(img_vol, box_blur)
 
+call_error_text("pyifx.graphics.convolute_custom", ['s', sobel_horizontal_np])
+call_error_text("pyifx.graphics.convolute_custom", [img1, 's'])
+call_error_text("pyifx.graphics.convolute_custom", [img1, sobel_horizontal_np, 's'])
+
 # Misc
 
 set_paths("imgs/misc/combine/")
 
 pyifx.misc.combine(img1, img2, "imgs/misc/combine") #TBC
+
+call_error_test("pyifx.misc.combine", [img1, 's', "imgs/misc/combine"])
 
 set_paths("imgs/misc/class_functions/")
 
