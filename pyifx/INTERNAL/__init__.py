f471702506c2cbe117c46639b675689d81d73e86
video-lab <32363370+Video-Lab@users.noreply.github.com>
2019-07-24 18:49:59 -0500

    Move all internal functions to one file

--- pyifx/INTERNAL/__init__.py
+++ pyifx/INTERNAL/__init__.py
@@ -1,5 +1,1 @@
-from pyifx.INTERNAL.INTERNAL import *
-from pyifx.INTERNAL.INTERNAL_hsl import *
-from pyifx.INTERNAL.INTERNAL_misc import *
-from pyifx.INTERNAL.INTERNAL_graphics import *
-from pyifx.INTERNAL.INTERNAL_comp import *
+from pyifx.INTERNAL import INTERNAL
