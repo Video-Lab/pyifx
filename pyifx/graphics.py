f6614472edbc2b61a54242efba913c05cc60daf7
video-lab <32363370+Video-Lab@users.noreply.github.com>
2019-07-29 16:37:08 -0500

    Add documentation for blur functions

--- pyifx/graphics.py
+++ pyifx/graphics.py
@@ -2,6 +2,23 @@ import pyifx.INTERNAL as INTERNAL
 import pyifx.misc as misc
 
 def blur_gaussian(img_paths, radius=3, size=None, write=True):
+	"""blur_gaussian(img_paths, radius=3, size=None, write=True) 
+		Takes images(s) and blurs them using a gaussian kernel.
+
+		Parameters:
+		img_paths (pyifx.misc.PyifxImage, pyifx.misc.ImageVolume, list): The images to be converted.
+
+		radius (int): The radius of the gaussian kernel. If nothing is entered, this parameter will default to 3.
+
+		size (list, NoneType): The dimensions of the gaussian kernel. Must be entered in
+
+		write (bool): Whether to write the resized image(s). 
+
+		Returns:
+		PyifxImage instance (pyifx.misc.PyifxImage)
+		ImageVolume instance (pyifx.misc.ImageVolume)
+		List with elements of type PyifxImage (list)
+	"""
 	INTERNAL._type_checker(radius, [int, float])
 	INTERNAL._type_checker(img_paths, [misc.PyifxImage, misc.ImageVolume, list])
 	INTERNAL._type_checker(write, [bool])
@@ -10,6 +27,21 @@ def blur_gaussian(img_paths, radius=3, size=None, write=True):
 	return INTERNAL._blur_handler(img_paths, radius=radius, type_kernel="gaussian", size=size, write=write)
 
 def blur_mean(img_paths, radius=3, write=True):
+	"""blur_mean(img_paths, radius=3, write=True) 
+		Takes images(s) and blurs them using a mean kernel.
+
+		Parameters:
+		img_paths (pyifx.misc.PyifxImage, pyifx.misc.ImageVolume, list): The images to be converted.
+
+		radius (int): The radius of the mean kernel. If nothing is entered, this parameter will default to 3.
+
+		write (bool): Whether to write the resized image(s). 
+
+		Returns:
+		PyifxImage instance (pyifx.misc.PyifxImage)
+		ImageVolume instance (pyifx.misc.ImageVolume)
+		List with elements of type PyifxImage (list)
+	"""
 	INTERNAL._type_checker(radius, [int])
 	INTERNAL._type_checker(img_paths, [misc.PyifxImage, misc.ImageVolume, list])
 	INTERNAL._type_checker(write, [bool])
