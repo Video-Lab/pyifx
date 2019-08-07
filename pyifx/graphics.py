776266f963de7543504eb872ef00343216f72427
Jad Khalili <jad@videolab.ae>
2019-08-06 19:51:53 -0500

    Format docstrings - Graphics module

--- pyifx/graphics.py
+++ pyifx/graphics.py
@@ -3,23 +3,24 @@ import pyifx.misc as misc
 import numpy as np
 
 def blur_gaussian(img_paths, radius=3, size=None, write=True):
-	"""blur_gaussian(img_paths, radius=3, size=None, write=True) 
+	""" blur_gaussian(img_paths, radius=3, size=None, write=True) 
 		Takes images(s) and blurs them using a gaussian kernel based on a given radius.
 
-		Parameters:
-		img_paths (pyifx.misc.PyifxImage, pyifx.misc.ImageVolume, list): The image(s) to be blurred.
+		:type img_paths: pyifx.misc.PyifxImage, pyifx.misc.ImageVolume, list
+		:param img_paths: The image(s) to be blurred.
 
-		radius (int): The radius of the gaussian kernel. If nothing is entered for this parameter,
-		it will default to 3.
+		:type radius: int
+		:param radius: The radius of the gaussian kernel. If nothing is entered for this parameter, it will default to 3.
 
-		size (list, NoneType): The dimensions of the gaussian kernel. Must be entered in
+		:type size: list, NoneType
+		:param size: The dimensions of the gaussian kernel. Must be entered in
 
-		write (bool): Whether to write the blurred image(s). 
+		:type write: bool
+		:param write: Whether to write the blurred image(s). 
 
-		Returns:
-		PyifxImage instance (pyifx.misc.PyifxImage)
-		ImageVolume instance (pyifx.misc.ImageVolume)
-		List with elements of type PyifxImage (list)
+		:return: PyifxImage instance, ImageVolume instance, or list with elements of type PyifxImage
+		:rtype: pyifx.misc.PyifxImage, pyifx.misc.ImageVolume, list
+
 	"""
 	INTERNAL._type_checker(radius, [int, float])
 	INTERNAL._type_checker(img_paths, [misc.PyifxImage, misc.ImageVolume, list])
@@ -29,20 +30,21 @@ def blur_gaussian(img_paths, radius=3, size=None, write=True):
 	return INTERNAL._convolution_handler(img_paths, radius=radius, type_kernel="gaussian", size=size, custom=None, write=write)
 
 def blur_mean(img_paths, radius=3, write=True):
-	"""blur_mean(img_paths, radius=3, write=True) 
+	""" blur_mean(img_paths, radius=3, write=True) 
 		Takes images(s) and blurs them using a mean kernel based on a given radius.
 
-		Parameters:
-		img_paths (pyifx.misc.PyifxImage, pyifx.misc.ImageVolume, list): The image(s) to be blurred.
+		:type img_paths: pyifx.misc.PyifxImage, pyifx.misc.ImageVolume, list
+		:param img_paths: The image(s) to be blurred.
 
-		radius (int): The radius of the mean kernel. If nothing is entered for this parameter, it will default to 3.
+		:type radius: int
+		:param radius: The radius of the mean kernel. If nothing is entered for this parameter, it will default to 3.
 
-		write (bool): Whether to write the blurred image(s). 
+		:type write: bool
+		:param write: Whether to write the blurred image(s). 
 
-		Returns:
-		PyifxImage instance (pyifx.misc.PyifxImage)
-		ImageVolume instance (pyifx.misc.ImageVolume)
-		List with elements of type PyifxImage (list)
+		:return: PyifxImage instance, ImageVolume instance, or list with elements of type PyifxImage
+		:rtype: pyifx.misc.PyifxImage, pyifx.misc.ImageVolume, list	
+
 	"""
 	INTERNAL._type_checker(radius, [int])
 	INTERNAL._type_checker(img_paths, [misc.PyifxImage, misc.ImageVolume, list])
@@ -51,21 +53,20 @@ def blur_mean(img_paths, radius=3, write=True):
 	return INTERNAL._convolution_handler(img_paths, radius=radius, type_kernel="mean", size=None, custom=None, write=write)
 
 def pixelate(img_paths, factor=4, write=True):
-	"""pixelate(img_paths, factor=4, write=True)
+	""" pixelate(img_paths, factor=4, write=True)
 		Takes image(s) and pixelates them based on a given factor.
 
-		Parameters:
-		img_paths (pyifx.misc.PyifxImage, pyifx.misc.ImageVolume, list): The image(s) to be pixelated.
+		:type img_paths: pyifx.misc.PyifxImage, pyifx.misc.ImageVolume, list
+		:param img_paths: The image(s) to be pixelated.
 
-		factor (int): How much the image(s) should be pixelated. If nothing is entered for this parameter,
-		it will default to 4.
+		:type factor: int
+		:param factor: How much the image(s) should be pixelated. If nothing is entered for this parameter, it will default to 4.
 
-		write (bool): Whether to write the pixelated image(s). 
+		:type write: bool
+		:param write: Whether to write the pixelated image(s). 
 
-		Returns:
-		PyifxImage instance (pyifx.misc.PyifxImage)
-		ImageVolume instance (pyifx.misc.ImageVolume)
-		List with elements of type PyifxImage (list)
+		:return: PyifxImage instance, ImageVolume instance, or list with elements of type PyifxImage
+		:rtype: pyifx.misc.PyifxImage, pyifx.misc.ImageVolume, list	
 
 	"""
 	INTERNAL._type_checker(factor, [int])
@@ -75,18 +76,17 @@ def pixelate(img_paths, factor=4, write=True):
 	return INTERNAL._pixelate_handler(img_paths, factor, write=write)
 
 def detect_edges(img_paths, write=True):
-	"""detect_edges(img_paths, write=True)
-			Takes image(s) and creates new images focusing on edges.
+	""" detect_edges(img_paths, write=True)
+		Takes image(s) and creates new images focusing on edges.
 
-			Parameters:
-			img_paths (pyifx.misc.PyifxImage, pyifx.misc.ImageVolume, list): The image(s) to be manipulated.
+		:type img_paths: pyifx.misc.PyifxImage, pyifx.misc.ImageVolume, list 
+		:param img_paths: The image(s) to be manipulated.
 
-			write (bool): Whether to write the manipulated image(s).					
+		:type write: bool
+		:param write: Whether to write the manipulated image(s).
 
-			Returns:
-			PyifxImage instance (pyifx.misc.PyifxImage)
-			ImageVolume instance (pyifx.misc.ImageVolume)
-			List with elements of type PyifxImage (list)
+		:return: PyifxImage instance, ImageVolume instance, or list with elements of type PyifxImage
+		:rtype: pyifx.misc.PyifxImage, pyifx.misc.ImageVolume, list	
 
 	"""
 	INTERNAL._type_checker(img_paths, [misc.PyifxImage, misc.ImageVolume, list])
@@ -95,21 +95,20 @@ def detect_edges(img_paths, write=True):
 	return INTERNAL._detect_edges_handler(img_paths, write=write)
 
 def convolute_custom(img_paths, kernel, write=True):
-	"""convolute_custom(img_paths, kernel, write=True)
+	""" convolute_custom(img_paths, kernel, write=True)
 		Takes image(s) and creates new images that are convoluted over using a given kernel.
 
-		Parameters:
-		img_paths (pyifx.misc.PyifxImage, pyifx.misc.ImageVolume, list): The image(s) to be convoluted over.
+		:type img_paths: pyifx.misc.PyifxImage, pyifx.misc.ImageVolume, list
+		:param img_paths: The image(s) to be convoluted over.
 
-		kernel (numpy.ndarray, list): The kernel to be used for convolution. This can be provided in either a 2-dimensional
-		list or a numpy 2-dimensional array.
+		:type kernel: numpy.ndarray, list 
+		:param kernel: The kernel to be used for convolution. This can be provided in either a 2-dimensional list or a numpy 2-dimensional array.
 
-		write (bool): Whether to write the convoluted image(s).					
+		:type write: bool 
+		:param write: Whether to write the convoluted image(s).
 
-		Returns:
-		PyifxImage instance (pyifx.misc.PyifxImage)
-		ImageVolume instance (pyifx.misc.ImageVolume)
-		List with elements of type PyifxImage (list)
+		:return: PyifxImage instance, ImageVolume instance, or list with elements of type PyifxImage
+		:rtype: pyifx.misc.PyifxImage, pyifx.misc.ImageVolume, list
 	"""
 	INTERNAL._type_checker(kernel, [np.ndarray, list])
 	INTERNAL._type_checker(img_paths, [misc.PyifxImage, misc.ImageVolume, list])
