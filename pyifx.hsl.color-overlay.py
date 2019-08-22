1240d5964d40183cc743f0a5a33b09b8dff530e0
Jad Khalili <jad@videolab.ae>
2019-08-21 20:30:39 -0500

    Move color overlay test

--- pyifx.hsl.color-overlay.py
+++ pyifx.hsl.color-overlay.py
@@ -1,2 +1,14 @@
 from test_vars import *
 set_paths("../tests/imgs/hsl/color-overlay")
+
+pyifx.hsl.color_overlay(img1, [255,0,0], 100)
+pyifx.hsl.color_overlay(img_vol, [0,255,0], 100)
+pyifx.hsl.color_overlay(img_list, [0,0,255], 100)
+
+call_error_test("pyifx.hsl.color_overlay", ["asdf", [255,0,0], 60])
+call_error_test("pyifx.hsl.color_overlay", [img1, [255,0], 60])
+call_error_test("pyifx.hsl.color_overlay", [img1, [255,0, 'e'], 60])
+call_error_test("pyifx.hsl.color_overlay", [img1, [255,0,0], "s"])
+call_error_test("pyifx.hsl.color_overlay", [img1, [255,0,0], 200])
+call_error_test("pyifx.hsl.color_overlay", [img1, [255,0], -10])
+call_error_test("pyifx.hsl.color_overlay", [img1, [255,0,0], 60, "s"])
