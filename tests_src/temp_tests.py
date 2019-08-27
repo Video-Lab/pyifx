81a5c1e437e74aec39455a4361f0f15ea8ec0c96
Jad Khalili <jad@videolab.ae>
2019-08-26 21:19:49 -0500

    Add temp tests file & write tests

---
+++ tests_src/temp_tests.py
@@ -0,0 +1,12 @@
+"""This file contains tests for new features/changes that haven't been designated to a file yet. Feel free to use this function as a playground to test your changes & additions."""
+
+
+from test_vars import *
+import sys
+set_paths("../tests/imgs/misc/PyifxImage/set_input_path")
+sys.path.append("../tests/imgs/")
+
+img1.set_input_path("512x512-jpeg-1.jpg")
+img1.set_output_path("OUT-512x512-jpeg-1.jpg")
+
+pyifx.hsl.darken(img1, 100)
