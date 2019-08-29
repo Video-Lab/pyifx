f71c56819a79f8f0542e40587658132bdb0d8414
Jad Khalili <jad@videolab.ae>
2019-08-29 18:47:08 -0500

    Change refresh volume -> volume to list

--- tests_src/pyifx.misc.ImageVolume.creation.py
+++ tests_src/pyifx.misc.ImageVolume.creation.py
@@ -1,9 +1,10 @@
 from test_vars import *
 
+img_vol.set_input_path("../tests/misc/ImageVolume/creation/")
 print(img_vol.get_volume())
 
-img_vol.refresh_volume(2)
+img_vol.volume_to_list(2)
 print(img_vol.get_volume())
 
-img_vol.refresh_volume(3)
+img_vol.volume_to_list(3)
 print(img_vol.get_volume())
